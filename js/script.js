dd792d7605a4af14500ab682ce1d9adecffdff5b
jmndz <janaemilyn@gmail.com>
2022-07-22 23:50:33 +0800

    added clear function; color on hover

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 const screen = document.querySelector('.screen-center');
-const grid = document.querySelectorAll('.grid-item');
+screen.addEventListener('click', sketch);
 const slider = document.querySelector('.slider');
 slider.addEventListener('mouseup', changeGrid);
 const sizeText = document.querySelector('.footer-center-text');
-//screen wxh 500x350
+const colorPicker = document.querySelector('#color-button');
+const gridChangeColor = document.querySelector('.grid-changeColor');
+const clearButton = document.querySelector('#clear-btn');
+clearButton.addEventListener('click', clear);
+
 const defaultSize = 16;
 let newSize = 0;
 let tempSize = 0;
+let sketchClick = false;
+
+function sketch(){
+    const grid = document.querySelectorAll('.grid-item');
+
+    if(sketchClick === false) sketchClick = true;
+    else sketchClick = false;
+    console.log(sketchClick);
+    
+    grid.forEach( item => item.addEventListener('mouseenter', function () {
+        if(sketchClick === true){
+            gridChangeColor.style.backgroundColor = colorPicker.value;
+            this.classList.add('grid-changeColor');
+        }
+    }))
+    
+}
+
+function clear(){
+    const grid = document.querySelectorAll('.grid-item');
+
+    grid.forEach(function(item){
+        item.classList.remove('grid-changeColor');
+    })
+}
 
 function makeGrid(size){
     console.log("New size: " + size + " Previous size: "+tempSize)
